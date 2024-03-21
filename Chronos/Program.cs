ce2d09ad79dcdf5f5a3549d5a7db17684bd41ccb
João Nogueira <joaonogueira.art@gmail.com>
2024-03-21 23:15:00 +0000

    Added first stopwatch

---
+++ Chronos/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace Chronos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch crono1 = new Stopwatch();
+            Stopwatch crono2 = new Stopwatch();
+            crono1.Start();
+            Thread.Sleep(600);
+
+            float time1 = crono1.ElapsedMilliseconds/1000f;
+
+            Console.WriteLine($"{time1:f3}");
+
+            crono1.Stop();
+
+        }
+    }
+}

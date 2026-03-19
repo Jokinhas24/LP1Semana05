9148009bc9c7bfdd2d823f5416c2ce2a319d0404
[João Nogueira] <146846348+Jokinhas24@users.noreply.github.com>
2026-03-19 16:16:06 +0000

    Add damage methods

---
+++ DungeonStats/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace DungeonStats
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello LP!");
+        }
+        private static int Damage(int attack, int defense)
+        {
+            if (defense > 0)
+            {
+                defense = 0;
+            }
+            return attack - defense;
+        }
+        private static int Damage(int attack)
+        {
+            return attack;
+        }
+        private static int CriticalHit(int damage)
+        {
+            if (damage <= 0)
+                damage = 0;
+            else
+                damage = 1 + CriticalHit(damage - 1);
+            return damage;
+        }
+    }
+}
